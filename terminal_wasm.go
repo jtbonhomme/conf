76044ac9b9d908c5a50d2b2db2386dbb950041f4
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-04-24 15:32:31 +0200

    fix: stub terminal for wasm

--- terminal_wasm.go
+++ terminal_wasm.go
@@ -6,14 +6,7 @@
 
 package conf
 
-import (
-        "syscall"
-        "unsafe"
-)
-
 // isTerminal returns true if the given file descriptor is a terminal.
 func isTerminal(fd int) bool {
-	var termios syscall.Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-	return err == 0
+    return false
 }
